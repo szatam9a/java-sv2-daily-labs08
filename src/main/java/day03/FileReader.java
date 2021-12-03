f2f17d2944eb50345dbf4bda53bc3373f0368c50
Szalay Tamás <szalaytamas90@gmail.com>
2021-12-03 17:13:12 +0100

    Footbaal and Temperature

--- src/main/java/day03/FileReader.java
+++ src/main/java/day03/FileReader.java
@@ -1,46 +1,83 @@
 package day03;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileReader {
+    private List<String> file = new ArrayList<>();
 
-
-    public int findSmallestTemperatureSpread(String filename) {
-        List<String> file;
+    public List<String> readIn(Path path) {
         try {
-            file = Files.readAllLines(Path.of(filename));
+            file = Files.readAllLines(path);
+            return file;
         } catch (IOException io) {
-            throw new IllegalStateException("no file to read", io);
+            throw new IllegalStateException("no file to read");
         }
-        removeIrrelevantData(file);
-        return readIntoData(file);
     }
 
+    public int findSmallestTemperatureSpread(Path path) {
 
-    public void removeIrrelevantData(List<String> file){
-        file.remove(0);
-        file.remove(0);
-        file.remove(file.size() - 1);
-    }
-    public int readIntoData(List<String> file) {
-        WeaterData weaterData = new WeaterData();
-        int resultday=0;
-        int minDiff = Integer.MAX_VALUE;
-        int min;
+        readIn(path);
         int day;
+        int min;
         int max;
-
-        for (String line : file
+        int minDiff = Integer.MAX_VALUE;
+        int resultday =0;
+        List<String> filtered =removeLines(file, 0,1,file.size()-1);
+        for (String line : filtered
         ) {
-            weaterData.readInData(line);
-            weaterData.checkTheDay();
+            min = Integer.parseInt(line.substring(12, 14));
+            max = Integer.parseInt(line.substring(6, 8));
+            day = Integer.parseInt(line.substring(2, 4).trim());
+
+            if (doReplace(max, min, minDiff)) {
+                resultday = day;
+                minDiff= max-min;
+            }
         }
-        return weaterData.getResultDay();
+        return resultday;
     }
 
+    public String findSmallestDifferent(Path path) {
+        readIn(path);
+        String team;
+        int f;
+        int a;
+        int minDiff = Integer.MAX_VALUE;
+        String theWinner = null;
+        List<String> filtered =removeLines(file, 0,18);
+        for (String line : filtered
+        ) {
+            f = Integer.parseInt(line.substring(43, 45));
+            a = Integer.parseInt(line.substring(50, 52));
+            team = line.substring(7, 23).trim();
+
+            if (doReplace(a, f, minDiff)) {
+                theWinner = team;
+                minDiff= Math.abs(a-f);
+            }
+        }
+        return theWinner;
+    }
+
+    public boolean doReplace(int first, int second, int minDiff) {
+        if (Math.abs(first - second) < minDiff) {
+            return true;
+        } else
+            return false;
+    }
+    public List<String> removeLines(List<String> lines,Integer... filtered){
+        List<String> filteredList = new ArrayList<>();
+        List<Integer> filers = Arrays.asList(filtered);
+        for (int i= 0; i<lines.size();i++)
+            if (filers.contains(i)){
+
+            } else
+                filteredList.add(lines.get(i));
+    return filteredList;}
 }
 
--- src/test/java/day03/FileReaderTest.java
+++ src/test/java/day03/FileReaderTest.java
@@ -1,14 +1,23 @@
 package day03;
 
+import day03.FileReader;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileReaderTest {
-
     @Test
-    void findSmalestTempertaureSpreadTest() {
-        FileReader fl = new FileReader();
-        System.out.println(fl.findSmallestTemperatureSpread("src/test/resources/datamunging/weather.dat"));
+    void findSmallestDifferentTest() {
+        FileReader fr = new FileReader();
+        String winner= fr.findSmallestDifferent(Path.of("src/test/resources/datamunging/footbaal.dat"));
+        assertEquals("Aston_Villa",winner);
+    }
+    @Test
+    void findSmallestTemperatureSpreadTest() {
+        FileReader fr = new FileReader();
+                int day = fr.findSmallestTemperatureSpread(Path.of("src/test/resources/datamunging/weather.dat"));
+        assertEquals(14,day);
     }
 }
